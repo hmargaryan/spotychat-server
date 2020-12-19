6506df92b3407661d1184459953463e25cd9f9f3
khotsevich <xozewitc@yandex.ru>
2020-12-19 18:55:14 +0300

    Добавил логику чата

--- src/index.js
+++ src/index.js
@@ -1,50 +1,39 @@
-// import { Server } from 'socket.io'
+import { Server } from 'socket.io'
 import http from 'http'
 import * as mongodb from './mongodb.js'
 import { config } from './config/config.js'
 import { app } from './app.js'
-// import Chat from './models/Chat.js'
+import { Chat } from './models/Chat.js'
 
 const server = http.createServer(app)
-// const io = new Server(server)
+const io = new Server(server)
 
-// io.on('connection', socket => {
-//   console.log('User is connected')
+io.on('connection', socket => {
+  console.log('User is connected')
 
-//   socket.on('joinToChat', async ({ chatId, userIds }) => {
-//     console.log('joinToChat', userIds)
-//     let chat
-//     chat = await Chat.findById(chatId)
-//     if (!chat) {
-//       chat = await Chat.create({
-//         users: userIds
-//       })
-//     }
-//     socket.join(chat.id)
-//   })
+  socket.on('join', async ({ chatId, users }) => {
+    console.log(`User is join to ${chatId}`)
+    const chat = await Chat.findByIdAndUpdate(
+      chatId,
+      { $set: { users } },
+      { upsert: true, new: true }
+    )
+    socket.join(chat.id)
+  })
 
-//   socket.on('joinToApp', async ({ userId }) => {
-//     console.log('joinToApp', userId)
-//     socket.join(userId)
-//   })
+  socket.on('sendMessage', async ({ chatId, owner, text }, callback) => {
+    console.log(`User send message ${text} to ${chatId}`)
+    const chat = await Chat.findById(chatId)
+    chat.messages = [...chat.messages, { text, owner }]
+    await chat.save()
+    io.to(chatId).emit('message', { text, owner })
+    callback()
+  })
 
-//   socket.on(
-//     'sendMessage',
-//     async ({ chatId, interlocutorId, owner, text }, callback) => {
-//       console.log(chatId, interlocutorId, owner, text)
-//       const chat = await Chat.findById(chatId)
-//       chat.messages = [...chat.messages, { text, owner }]
-//       await chat.save()
-//       io.to(interlocutorId).emit('userMessage', { text, owner, chatId })
-//       io.to(chatId).emit('message', { text, owner })
-//       callback()
-//     }
-//   )
-
-//   socket.on('disconnect', () => {
-//     console.log('User is disconnected')
-//   })
-// })
+  socket.on('disconnect', () => {
+    console.log('User is disconnected')
+  })
+})
 
 mongodb
   .connect()
